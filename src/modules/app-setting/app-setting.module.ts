8283ae5cfab9628cfffdc806deaef92f86d52c7f
Muhammad Dede <m.dedenuraen@gmail.com>
2025-09-08 12:53:38 +0700

    refactor:remove upload module

--- src/modules/app-setting/app-setting.module.ts
+++ src/modules/app-setting/app-setting.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppSettingService } from './app-setting.service';
 import { AppSettingController } from './app-setting.controller';
 import { PrismaModule } from '@/prisma/prisma.module';
-import { UploadModule } from '@/upload/upload.module';
 
 @Module({
-  imports: [PrismaModule, UploadModule],
+  imports: [PrismaModule],
   providers: [AppSettingService],
   controllers: [AppSettingController],
 })
